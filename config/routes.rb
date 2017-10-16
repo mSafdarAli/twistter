cf551858471fdec80332ac81abab6e2093b28ec6
M Safdar Ali Khan <msafdaralikhan65@gmail.com>
2017-10-17 00:42:47 +0500

    Completed the twitter clone!

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  
   devise_for :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   resources :posts
+  # Define Root URL
   root 'pages#index'
-
+  
+  # Define Routes for Pages
   get '/home' => 'pages#home' # override default routes.
   get '/user/:id' => 'pages#profile'
   get '/explore' => 'pages#explore'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
